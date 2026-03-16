344dbefca2169e1aa4db3bb372fe087a0a63c78f
MiruthuMurugesh123 <miruthubhashini456@gmail.com>
2026-03-16 14:54:19 +0530

    Update program1.C

--- program1.C
+++ program1.C
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int n, i, a;
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        printf("Student %d (1=Present,0=Absent): ", i);
+        scanf("%d", &a);
+
+        if (a == 1)
+            printf("Present\n");
+        else
+            printf("Absent\n");
+    }
+    return 0;
+}
